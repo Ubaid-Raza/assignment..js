6c299ad9e038eeb0bffcc4f40a57880d430a1fda
Ubaid-Raza <79793357+Ubaid-Raza@users.noreply.github.com>
2022-11-02 04:11:30 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+window.alert("Well Come To Project");
+var a1 = "abc";
+var b2 = "xyz";
+ console.log (a1); 
+ console.log (b2);
+
+
+ var e = 20;
+ var f = 10;
+ console.log(e - f);
+
+ var c = 20;
+ var d = 10;
+ console.log(c * d);
+
+
+ var g = 20;
+ var h = 10;
+ console.log(g / h);
+ 
+ var ii = 20;
+ var jj = 10;
+ console.log(i, j);
+
+
+
+ var i = 20;
+ var j = 10;
+ console.log(i + j);
+
+ var k = 100;
+ var l =k++;
+ console.log(k)
+ 
+ 
+
+ var o = 100;
+ var p =++o;
+ console.log(o)
+
+ var q = 100;
+ var r =--q;
+ console.log(q)
+
+ var m = 100;
+ var n =m--;
+ console.log(m)
+
+ var s =100;
+ var t =10;
+ console.log(s*t+s-t);
